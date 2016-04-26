ffe9434c3d6de9dff6ab62e38e30c511846fd80d
lucashg94 <lucashg94@hotmail.es>
2016-04-27 01:27:39 +0200

    pruebas

--- VallaBot.py
+++ VallaBot.py
@@ -17,9 +17,9 @@ def hello():
         # print request.data #int(request.data["message"]["chat"]["id"]) 23709664
         #telegram.Bot(TOKEN).sendMessage(chat_id=23709664, text=request.data)
 
-        #update = telegram.Update.de_json(request.get_json(force=True))
-        #chat_id = update.message.chat.id
-        telegram.Bot(TOKEN).sendMessage(chat_id=23709664, text="La teperatura es: "+tiempo())
+        update = telegram.Update.de_json(request.get_json(force=True))
+        chat_id = update.message.chat.id
+        telegram.Bot(TOKEN).sendMessage(chat_id=chat_id, text="La teperatura es: "+tiempo())
         #telegram.Bot(TOKEN).sendMessage(chat_id=chat_id, text="La temperatura es: "+str(tiempo()))
     return "Hola"
 
