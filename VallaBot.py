6aaebea010a7d3e3010a156a3ef2c52d5da1f56a
lucashg94 <lucashg94@hotmail.es>
2016-04-26 16:42:10 +0200

    funcionando en local

--- VallaBot.py
+++ VallaBot.py
@@ -4,18 +4,20 @@ import json
 import telegram
 import logging
 from flask import Flask
+app = Flask(__name__)
 
 @app.route("/mensaje")
 def hello():
     print request.data
     return "Hello World!"
 
-if __name__=="__main__":
-    app.run()
-    telebot()
-
 
 def telebot():
     TOKEN = '206483377:AAHnQ_ohMuvDhI5mfbDMrHKTnTGIi7YhT6A' # Ponemos nuestro Token generado con el @BotFather
     bot = telegram.Bot(TOKEN)
     bot.setWebhook('https://vallbot.herokuapp.com/mensaje')
+
+
+if __name__=="__main__":
+    app.run()
+    telebot()
