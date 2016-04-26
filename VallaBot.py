6d07dcfb207158da8f34cf4e8d379a893597966d
lucashg94 <lucashg94@hotmail.es>
2016-04-26 17:09:37 +0200

    puerto mejorad

--- VallaBot.py
+++ VallaBot.py
@@ -25,5 +25,5 @@ def telebot():
 
 if __name__=="__main__":
     print("empiezo")
-    app.run(debug=True,port=int(os.environ.get("PORT",5000)))
+    app.run(host='0.0.0.0',debug=True,port=int(os.environ.get("PORT",5000)))
     telebot()
