5fd0bf426ec0c42740d8dddf23e51b7b5342f124
lucashg94 <lucashg94@hotmail.es>
2016-04-27 02:18:33 +0200

    pruebas

--- VallaBot.py
+++ VallaBot.py
@@ -34,11 +34,11 @@ def tiempo():
     opener = urllib2.build_opener()
     f = opener.open(req)
     j = json.loads(f.read())
-    return "Actual: "+ str(j["currently"]["temperature"])+"ºC, "\
-        +str(j["currently"]["summary"])+" \nMáxima: "\
-        +str(j["daily"]["data"][0]["temperatureMax"])+"ºC\nMínima: "\
-        +str(j["daily"]["data"][0]["temperatureMin"])+"ºC\nResumen: "\
-        +str(j["daily"]["summary"])
+    return "Actual: "+ str(j["currently"]["temperature"])+"ºC"#, "\
+        #+str(j["currently"]["summary"])+" \nMáxima: "\
+        #+str(j["daily"]["data"][0]["temperatureMax"])+"ºC\nMínima: "\
+        #+str(j["daily"]["data"][0]["temperatureMin"])+"ºC\nResumen: "\
+        #+str(j["daily"]["summary"])
 
 if __name__=="__main__":
     print("empiezo")
